1115df77f9c3294982923e9dbe3c6ee478a94b98
Valérian Sibille <valerian.sibille@meetiqm.com>
2023-02-10 16:35:02 +0100

    Restrain FZF_DEFAULT_COMMAND to current directory

--- .fzf.zsh
+++ .fzf.zsh
@@ -5,7 +5,7 @@ if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
 fi
 
 if command -v fd >/dev/null 2>&1; then
-    export FZF_DEFAULT_COMMAND="fd . $HOME"
+    export FZF_DEFAULT_COMMAND="fd --type f ."
     export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
     export FZF_ALT_C_COMMAND="fd --type d . $HOME"
 fi
