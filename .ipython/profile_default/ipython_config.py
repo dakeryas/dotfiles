93f285ca7702e32b00af4c4087af1933518a10cf
Valérian Sibille <valerian.sibille@meetiqm.com>
2023-03-08 11:05:26 +0100

    Add ipython_config

---
+++ .ipython/profile_default/ipython_config.py
@@ -0,0 +1,7 @@
+from IPython.core.ultratb import VerboseTB
+VerboseTB._tb_highlight = "bg:#039dfc"
+
+c.TerminalInteractiveShell.editing_mode = 'vi'
+c.TerminalInteractiveShell.editor = u'vim'
+c.TerminalInteractiveShell.extra_open_editor_shortcuts = True
+c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
