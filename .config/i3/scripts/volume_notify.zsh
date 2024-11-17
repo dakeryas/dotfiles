4634bf0e186f4327c9c472d464fb1f1de7561cc6
Valérian Sibille <vsibille@mit.edu>
2024-11-17 21:14:27 +0100

    Remove last character from  pactl volume string

--- .config/i3/scripts/volume_notify.zsh
+++ .config/i3/scripts/volume_notify.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 #
 ICON_PATH=~/.local/share/icons/volume.svg \
-    READ_CMD="pactl get-sink-volume @DEFAULT_SINK@ | awk '/Vol/{print \$5}'" \
+    READ_CMD="pactl get-sink-volume @DEFAULT_SINK@ | awk '/Vol/{print substr(\$5,0, length(\$5)-1)}'" \
     TAG=Volume \
     ${0:A:h}/brightness_notify.zsh
