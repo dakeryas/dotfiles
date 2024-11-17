045be532cb05b78d1785860fa2b045a9a3d2756f
Valérian Sibille <vsibille@mit.edu>
2024-11-17 21:58:59 +0100

    Change icon volume if muted

--- .config/i3/scripts/volume_notify.zsh
+++ .config/i3/scripts/volume_notify.zsh
@@ -1,6 +1,10 @@
 #!/usr/bin/env zsh
-#
-ICON_PATH=~/.local/share/icons/volume.svg \
+
+icon=~/.local/share/icons/volume.svg
+mute=$(pactl get-sink-mute @DEFAULT_SINK@)
+[[ $mute == *yes* ]] && icon=~/.local/share/icons/silent.svg
+
+ICON_PATH=$icon \
     READ_CMD="pactl get-sink-volume @DEFAULT_SINK@ | awk '/Vol/{print substr(\$5,0, length(\$5)-1)}'" \
     TAG=Volume \
     ${0:A:h}/brightness_notify.zsh
