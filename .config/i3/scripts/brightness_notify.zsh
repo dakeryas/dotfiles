62c06772358b3b46fc5d8b38c1589d0d74f94cc2
Valérian Sibille <vsibille@mit.edu>
2024-11-17 20:06:25 +0100

    Made brightness_notify more generic

--- .config/i3/scripts/brightness_notify.zsh
+++ .config/i3/scripts/brightness_notify.zsh
@@ -1,12 +1,14 @@
 #!/usr/bin/env zsh
 
-ICON_PATH=$HOME/.local/share/icons/brightness.svg
-TIMEOUT=500
+: ${ICON_PATH:="$HOME/.local/share/icons/brightness.svg"}
+: ${TIMEOUT:=500}
+: ${TAG:=Brightness}
+: ${READ_CMD:="xbacklight -get"}
 
 function main(){
-    local bri=$(xbacklight -get);
-    local dunst_tag=string:x-dunst-stack-tag:brightness  # shared location for brightness tag
-    args=(-t $TIMEOUT -h $dunst_tag -h int:value:$bri Brightness "$bri%")
+    local val=$(eval $READ_CMD);
+    local dunst_tag=string:x-dunst-stack-tag:$TAG  # shared location for given tag
+    args=(-t $TIMEOUT -h $dunst_tag -h int:value:$val $TAG "$val%")
     [[ -f $ICON_PATH ]] && args+=(-i $ICON_PATH)
     notify-send $args
 }
